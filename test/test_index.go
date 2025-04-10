ff4083e33fb1eb236d03b21d8fbe4f4da7fe5d1b
xuyd <sssxyd@gmail.com>
2025-04-10 15:12:51 +0800

    1

--- test/test_index.go
+++ test/test_index.go
@@ -3,11 +3,11 @@ package test
 import (
 	"testing"
 
-	"github.com/sssxyd/go-browser-handle/src/handle" // 导入 src/handle 包
+	"github.com/sssxyd/go-browser-handle/handle"
 )
 
 func TestRemoveByValue(t *testing.T) {
-	edge, err := handle.StartEdgeBrowser("edgePath", 3000, 4000) // 使用 handle 包中的函数
+	edge, err := handle.StartEdgeBrowser("edgePath", 3000, 4000)
 	if err != nil {
 		t.Fatalf("Failed to start Edge browser: %v", err)
 	}
